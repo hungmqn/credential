7a7df6b92c3efd84b8c482b57303e53647f57fdd
Hung Nguyen <hungmqn@gmail.com>
2022-07-31 18:47:10 +0700

    task: add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,171 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { CSSProperties, ReactNode, useMemo, useState } from 'react';
+import { InboxOutlined, GithubOutlined } from '@ant-design/icons';
+import {
+  Layout,
+  Typography,
+  Upload,
+  UploadProps,
+  message,
+  Grid,
+  Tag,
+  Space,
+  Empty,
+} from 'antd';
+import { RcFile } from 'antd/lib/upload';
+import { Breakpoint } from 'antd/lib/_util/responsiveObserve';
 
-function App() {
+import DataScreen from './DataScreen';
+
+const Container = ({
+  screen,
+  children,
+  style = {},
+}: {
+  screen: Breakpoint;
+  children: ReactNode;
+  style?: CSSProperties;
+}) => {
+  const getWidthFromScreen = (scr: Breakpoint) => {
+    switch (scr) {
+      case 'xs':
+        return 480;
+      case 'sm':
+        return 576;
+      case 'md':
+        return 768;
+      case 'lg':
+        return 992;
+      case 'xl':
+        return 1200;
+      case 'xxl':
+        return 1600;
+      default:
+        return 992;
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{ maxWidth: getWidthFromScreen(screen), margin: 'auto', ...style }}
+    >
+      {children}
     </div>
   );
-}
+};
+
+const App = () => {
+  const [data, setData] = useState<Array<any>>([]);
+  const [selectedFile, setSelectedFile] = useState<undefined | RcFile>();
+  const { useBreakpoint } = Grid;
+  const screens = useBreakpoint();
+  const currentBreakPoint: Breakpoint = useMemo(
+    () =>
+      Object.entries(screens).reduce(
+        (prev, current) => (current[1] ? current[0] : prev) as Breakpoint,
+        'xs' as Breakpoint
+      ),
+    [screens]
+  );
+
+  const props: UploadProps = {
+    accept: '.json',
+    beforeUpload: (file: RcFile, _fileList: RcFile[]) => {
+      if (file) {
+        setSelectedFile(file);
+        const fileReader = new FileReader();
+        fileReader.onload = (e) => {
+          setData(JSON.parse(e.target?.result as string));
+          message.success(`Processing file: ${file.name}`);
+        };
+        fileReader.onerror = (e) =>
+          message.error(`Fail to read file, details: ${e.target?.result}`);
+
+        fileReader.readAsText(file);
+      }
+
+      return false;
+    },
+    style: {
+      marginBottom: 24,
+    },
+  };
+  return (
+    <div>
+      <Layout>
+        <Layout.Header
+          style={{
+            position: 'sticky',
+            zIndex: 1,
+            width: '100%',
+            background: '#fff',
+          }}
+        >
+          <Typography.Title style={{ textAlign: 'center' }}>
+            Credential
+          </Typography.Title>
+        </Layout.Header>
+        <Layout.Content>
+          <Container screen={currentBreakPoint} style={{ marginBottom: 32 }}>
+            <Space
+              direction="vertical"
+              size="large"
+              style={{ display: 'flex' }}
+            >
+              <div
+                style={{
+                  textAlign: 'center',
+                  paddingTop: 24,
+                  marginBottom: 24,
+                }}
+              >
+                <Typography.Text>
+                  Current break point: {currentBreakPoint}
+                </Typography.Text>
+                <br />
+                {Object.entries(screens)
+                  .filter((screen) => !!screen[1])
+                  .map((screen) => (
+                    <Tag color="blue" key={screen[0]}>
+                      {screen[0]}
+                    </Tag>
+                  ))}
+              </div>
+              <Upload.Dragger {...props}>
+                <p className="ant-upload-drag-icon">
+                  <InboxOutlined />
+                </p>
+                <p className="ant-upload-text">
+                  Click or drag file to this area to
+                </p>
+                <p className="ant-upload-hint">
+                  Requirements: Type: .json | Content: Array of objects. This
+                  will not send file to anywhere. If you refresh, close the tab
+                  or select new file, the data will be reloaded.
+                </p>
+              </Upload.Dragger>
+              <div>
+                {selectedFile && data ? (
+                  <DataScreen data={data} />
+                ) : (
+                  <Empty description="No data, please select data file." />
+                )}
+              </div>
+            </Space>
+          </Container>
+        </Layout.Content>
+        <Layout.Footer style={{ background: '#fff' }}>
+          <Typography.Title style={{ textAlign: 'center' }} level={5}>
+            <a
+              href="https://github.com/hungmqn/credential"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GithubOutlined style={{ fontSize: 32 }} />
+            </a>
+          </Typography.Title>
+        </Layout.Footer>
+      </Layout>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/DataScreen.tsx
@@ -0,0 +1,115 @@
+import {
+  flow,
+  flatMap,
+  keysIn,
+  flatten,
+  uniq,
+  without,
+  getOr,
+  map,
+  filter,
+  debounce,
+} from 'lodash/fp';
+import { useEffect, useMemo, useState } from 'react';
+import {
+  Typography,
+  Empty,
+  Form,
+  Checkbox,
+  Row,
+  Col,
+  Table,
+  Input,
+} from 'antd';
+import { ColumnType } from 'antd/lib/table';
+
+type DataScreenProps = {
+  data: Array<any>;
+};
+
+const getFields = flow(
+  flatMap((obj) => keysIn(obj)),
+  flatten,
+  uniq,
+  without(['key'])
+);
+
+const fieldsToColumns = flatMap((field) => ({
+  title: field,
+  dataIndex: field,
+  key: field,
+}));
+
+const processInitialData = (data: Array<any>) =>
+  data.map((dataRow, index) => ({
+    key: index.toFixed(2),
+    ...dataRow,
+  }));
+
+const searchData = (search: string) =>
+  flow(
+    map((record) => JSON.stringify(record)),
+    filter((value: string) => value.toLocaleLowerCase().indexOf(search) >= 0),
+    map((record) => JSON.parse(record))
+  );
+
+const DataScreen = ({ data = [] }: DataScreenProps) => {
+  const originData = useMemo(() => processInitialData(data), [data]);
+  const [filteredData, setFilteredData] = useState<any>();
+  const fields = useMemo(() => getFields(originData), [originData]);
+  const [form] = Form.useForm();
+  const selectedFields = Form.useWatch('selectedFields', form);
+  const columns = fieldsToColumns(selectedFields) as ColumnType<any>[];
+
+  const handleApplyFilter = (_: any, values: any) => {
+    const search = getOr('', 'search')(values);
+    setFilteredData(searchData(search.toLocaleLowerCase())(originData));
+  };
+
+  useEffect(() => {
+    setFilteredData(originData);
+  }, [originData]);
+
+  return data.length > 0 ? (
+    <div style={{ background: '#fff', padding: 24 }}>
+      <Form
+        form={form}
+        name="advanced_search"
+        className="ant-advanced-search-form"
+        initialValues={{
+          selectedFields: fields,
+        }}
+        onValuesChange={debounce(500, handleApplyFilter)}
+      >
+        <Typography.Title level={5} style={{ marginBottom: 24 }}>
+          Select fields to show
+        </Typography.Title>
+        <div>
+          <Form.Item name="selectedFields">
+            <Checkbox.Group>
+              <Row gutter={[24, 24]}>
+                {fields.length > 0 &&
+                  fields.map((field) => (
+                    <Col span={4} key={field}>
+                      <Checkbox value={field}>{field}</Checkbox>
+                    </Col>
+                  ))}
+              </Row>
+            </Checkbox.Group>
+          </Form.Item>
+          <Form.Item name="search">
+            <Input placeholder="Search for all fields" />
+          </Form.Item>
+        </div>
+        <Table dataSource={filteredData} columns={columns} />
+      </Form>
+    </div>
+  ) : (
+    <Empty
+      image={Empty.PRESENTED_IMAGE_SIMPLE}
+      description="The file you select doesn't contain proper data, please select another file."
+    />
+  );
+};
+
+export default DataScreen;
